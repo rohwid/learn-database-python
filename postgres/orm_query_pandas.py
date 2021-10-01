31cd4eae47e249e3f5a8e2307e9cf944e55fe958
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-10-01 16:27:25 +0700

    postgres docker and query

--- postgres/orm_query_pandas.py
+++ postgres/orm_query_pandas.py
@@ -1,12 +1,12 @@
 from liblearn import helper
 
 dir = '/home/rohwid/GitHub/learn-databases/postgres/files'
-env_list = ['pacmann', 'pacmann123', '0.0.0.0', '30000', 'service_db']
+env_list = ['learn', 'learn', '0.0.0.0', '5432', 'db_learn']
 table_name = "tb_coba"
 
 if __name__ == '__main__':
     helper.create_dataset_pandas(
-        f"{dir}/data_input_large.tsv", 
+        f"{dir}/entity_relation_dataset_large.tsv", 
         env_list, 
         table_name, 
         header=True, 
